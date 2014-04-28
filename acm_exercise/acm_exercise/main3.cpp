c8c596816bd8fc7ab5db1bf6eb5f7cffe0b13afd
heiyanbin <heiyanbin@163.com>
2014-04-28 16:50:16 +0800

    add the  calucate expression  question/solution

--- acm_exercise/acm_exercise/main3.cpp
+++ acm_exercise/acm_exercise/main3.cpp
@@ -1,4 +1,27 @@
-//
+//Dr.Kong设计的机器人卡多掌握了加减法运算以后，最近又学会了一些简单的函数求值，比如，它知道函数min(20,23)的值是20 ，add(10,98) 的值是108等等。经过训练，Dr.Kong设计的机器人卡多甚至会计算一种嵌套的更复杂的表达式。
+//假设表达式可以简单定义为:
+//1. 一个正的十进制数 x 是一个表达式。
+//2. 如果 x 和 y 是 表达式，则 函数min(x,y )也是表达式,其值为x,y 中的最小数。
+//3. 如果 x 和 y 是 表达式，则 函数max(x,y )也是表达式,其值为x,y 中的最大数。
+//4．如果 x 和 y 是 表达式，则 函数add(x,y )也是表达式,其值为x,y 之和。
+//例如， 表达式 max(add(1,2),7) 的值为 7。
+//请你编写程序，对于给定的一组表达式，帮助 Dr.Kong 算出正确答案，以便校对卡多计算的正误。
+//输入
+//第一行： N 表示要计算的表达式个数 （1≤ N ≤ 10） 
+//接下来有N行， 每行是一个字符串，表示待求值的表达式
+//（表达式中不会有多余的空格，每行不超过300个字符，表达式中出现的十进制数都不
+//超过1000。）
+//输出
+//输出有N行，每一行对应一个表达式的值。
+//样例输入
+//3
+//add(1,2) 
+//max(1,999) 
+//add(min(1,1000),add(100,99)) 
+//样例输出
+//3
+//999
+//200
 //  main3.cpp
 //  acm_exercise
 //
@@ -8,68 +31,83 @@
 
 #include <iostream>
 #include <vector>
+#include <cstring>
 using namespace std;
-int main()
+int main3()
 {
 	int n;
 	cin>>n;
 	char (*a)[300] = new char[n][300];
-    vector<char> op;
+    
+    char b[100];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
     vector<int> operand;
 	for(int i=0;i<n;i++)
 	{
-		gets(a[i]);
-	}
-	for(int i=0;i<n;i++)
-	{
-		char *p=a[i];
-        if(*p<'9' && *p>'0')
+		int len= strlen(a[i]);
+        int j =len-1;
+        while(j>=0)
         {
-            cout<<atoi(p);
-            continue;
-        }
-        bool lastPushNumber= false;
-		while(*p)
-		{
-			if(*p=='a')
+            if(a[i][j]<='9' && a[i][j]>='0')
+            {
+                int v=0,f=1;
+                while(j>=0 && a[i][j]<='9' && a[i][j]>='0')
+                {
+                    v=v+ f*(a[i][j]-'0');
+                    f=f*10; 
+                    j--;
+                }
+                operand.push_back(v);
+            }		   
+			else if(a[i][j]=='d')
 			{
-				p++;
-                if(*p!='d') throw invalid_argument("unknown op.");
-				p++;
-                if(*p!='d') throw invalid_argument("unknown op.");
-                op.push_back('+');
-                lastPushNumber = false;
-                p++;
+				j--;
+                if(a[i][j]!='d') throw invalid_argument("unknown op.");
+				j--;
+                if(a[i][j]!='a') throw invalid_argument("unknown op.");
+                int x = operand.back();
+                operand.pop_back();
+                int y = operand.back();
+                operand.pop_back();
+                operand.push_back(x+y);
+                j--;
 			}
-            else if(*p=='('||*p==')')
+            else if(a[i][j]=='x')
             {
-                p++;
-                //op.push_back('(');
+                j--;
+                if(a[i][j]!='a') throw invalid_argument("unkonw op");
+                j--;
+                if(a[i][j]!='m') throw invalid_argument("unknow op");
+                int x = operand.back();
+                operand.pop_back();
+                int y = operand.back();
+                operand.pop_back();
+                operand.push_back(max(x,y));
+                j--;
             }
-            else if(*p<'9' && *p>'0')
+            else if(a[i][j]=='n')
             {
-                int v =0;
-                while(*p<'9'&&*p>'0')
-                {
-                    v=v*10+*p-'0';
-                    p++;
-                }
-                if(!lastPushNumber)
-                {
-                    operand.push_back(v);
-                    lastPushNumber =true;
-                }
-                else{
-                    int tmp =  operand.back();
-                    operand.pop_back();
-                    char oper = op.back();
-                    op.pop_back();
-                    if(oper=='+')
-                        operand.push_back(tmp+v);
-                }
+                j--;
+                if(a[i][j]!='i') throw invalid_argument("unkonw op");
+                j--;
+                if(a[i][j]!='m') throw invalid_argument("unknow op");
+                int x = operand.back();
+                operand.pop_back();
+                int y = operand.back();
+                operand.pop_back();
+                operand.push_back(min(x,y));
+                j--;
             }
-			
+            else// if(a[i][j]=='('||a[i][j]==')'||a[i][j]==',')
+            {
+                j--;
+            }          	
         }
-        cout<<
+        cout<<operand.back()<<endl;
+        operand.pop_back();
     }
+    delete[] a;
 }
---
+++ acm_exercise/acm_exercise/main4.cpp
@@ -0,0 +1,71 @@
+﻿//描述
+//走进世博园某信息通信馆，参观者将获得前所未有的尖端互动体验，一场充满创想和喜悦的信息通信互动体验秀将以全新形式呈现，从观众踏入展馆的第一步起，就将与手持终端密不可分，人类未来梦想的惊喜从参观者的掌上展开。
+//
+//在等候区的梦想花园中，参观者便开始了他们奇妙的体验之旅，等待中的游客可利用手机等终端参与互动小游戏，与梦想剧场内的虚拟人物Kr. Kong 进行猜数比赛。当屏幕出现一个整数X时，若你能比Kr. Kong更快的发出最接近它的素数答案，你将会获得一个意想不到的礼物。
+//
+//例如：当屏幕出现22时，你的回答应是23；当屏幕出现8时，你的回答应是7；若X本身是素数，则回答X;若最接近X的素数有两个时，则回答大于它的素数。
+//
+// 
+//
+//输入
+//第一行：N 要竞猜的整数个数
+//接下来有N行，每行有一个正整数X
+//1<=N<=5 1<=X<=1000
+//输出
+//输出有N行，每行是对应X的最接近它的素数
+//样例输入
+//4
+//22
+//5
+//18
+//8
+//样例输出
+//23
+//5
+//19
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+bool isPrime(int x)
+{
+    int f = sqrt(x);
+    for(int i=2;i<=f;i++)
+    {
+        if(x%i==0)  return false;
+    }
+    return true;
+}
+int main()
+{
+  int n;
+  cin>>n;
+  if(n>5||n<1) throw exception();
+  int *a = new int[n];
+  for(int i=0;i<n;i++)
+  {  
+       cin>>a[i];
+       if(a[i]>1000||a[i]<1) throw exception();
+  }
+  for(int i=0;i<n;i++)
+  {
+      int x =a[i];
+      while(!isPrime(x))
+      {
+          x--;
+      }
+      if(x==a[i]) 
+      {
+          cout<<x<<endl;
+          continue;
+      }   
+      int y= a[i];
+      while(!isPrime(y))
+      {
+          y++;
+      }
+      cout<< (a[i]-x < y-a[i] ? x : y) <<endl;
+  }
+  delete [] a;
+}
+
