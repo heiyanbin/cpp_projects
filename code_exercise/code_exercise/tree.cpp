e8bd908c1c9b2764eb63dfc094d2d395a159a63e
heiyanbin <heiyanbin@163.com>
2014-05-18 12:38:37 +0800

    minor

--- code_exercise/code_exercise/tree.cpp
+++ code_exercise/code_exercise/tree.cpp
@@ -7,8 +7,7 @@ struct TreeNode
     T data;
     TreeNode *left;
     TreeNode * right;
-public:
-    TreeNode(T data) {this.data = data;}
+    TreeNode(T data1) {data = data1;}
 };
 vector<TreeNode<int>*> path;
 void findPath(TreeNode<int>* root, int sum)
