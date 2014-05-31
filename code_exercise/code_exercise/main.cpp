7917a6245dcbd60f5be895b3cbfdea948ed38f23
heiyanbin <heiyanbin@163.com>
2014-05-31 16:24:00 +0800

    some comments

--- code_exercise/code_exercise/main.cpp
+++ code_exercise/code_exercise/main.cpp
@@ -47,8 +47,51 @@ int main(int argc, const char * argv[])
     test();
 }
 
+
+  /**
+ * Definition for binary tree
+ */ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+      TreeNode(int x, TreeNode *l, TreeNode *r) : val(x), left(l), right(r) {}
+  };
+ 
+class Solution {
+public:
+    int maxPathSum(TreeNode *root) {
+        if(!root) return 0;
+        int globalMaxSum = root->val;
+        int x = maxPathSumFromRoot(root, globalMaxSum);
+        return globalMaxSum;
+    }
+    
+    int maxPathSumFromRoot(TreeNode * root, int &globalMaxSum)
+    {
+        if(root==NULL) return 0;
+        
+        int maxSum = root->val;
+        int leftSum = maxPathSumFromRoot(root->left,globalMaxSum)+root->val;
+        if(leftSum>maxSum)
+            maxSum = leftSum;
+        int rightSum = maxPathSumFromRoot(root->right,globalMaxSum)+root->val;
+        if( rightSum>maxSum)
+            maxSum = rightSum;
+        int allSum = leftSum - root->val + rightSum;
+        if(allSum > globalMaxSum)
+            globalMaxSum= allSum;
+        if(maxSum>globalMaxSum)
+            globalMaxSum = maxSum;
+        return maxSum;
+    }
+};
 void test()
 {
+   
+    TreeNode *root = new TreeNode(5,new TreeNode(4,new TreeNode(11,new TreeNode(7,NULL,new TreeNode(1)),new TreeNode(2)),NULL),new TreeNode(8,new TreeNode(13),new TreeNode(4)));
     
+    Solution s;
+    int x = s.maxPathSum(root);
 }
 
